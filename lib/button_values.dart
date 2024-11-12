a14d78e3158c3540100629e09613b4692d8314c1
arvievince <arvie.oct.hd@gmail.com>
2024-11-12 23:17:06 +0800

    Initial commit

---
+++ lib/button_values.dart
@@ -0,0 +1,47 @@
+class Btn{
+  static const String del = "Del";
+  static const String clr = "C";
+  static const String per = "%";
+  static const String multiply = "x";
+  static const String devide = "÷";
+  static const String subtract = "-";
+  static const String add = "+";
+  static const String equal = "=";
+  static const String dot = ".";
+//numbers
+  static const String num = "0";
+  static const String numz = "00";
+  static const String num1 = "1";
+  static const String num2 = "2";
+  static const String num3 = "3";
+  static const String num4 = "4";
+  static const String num5 = "5";
+  static const String num6 = "6";
+  static const String num7 = "7";
+  static const String num8 = "8";
+  static const String num9 = "9";
+
+  //list of const
+  static const List<String> buttonValues = [
+    del,
+    clr,
+    per,
+    devide,
+    num7,
+    num8,
+    num9,
+    multiply,
+    num4,
+    num5,
+    num6,
+    subtract,
+    num1,
+    num2,
+    num3,
+    add,
+    num,
+    numz,
+    dot,
+    equal,
+  ];
+}
